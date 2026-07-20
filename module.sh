2cf3fab4a33b931cb1906833a48f248568f91a34
Dark-Xploiter <nuril4983@gmail.com>
2026-07-20 19:47:22 +0700

    Initial commit / Update pertama

---
+++ module.sh
@@ -0,0 +1,58 @@
+apt install curl -y
+apt install python -y
+apt install python-pip -y #buat kali (karena package python & pip terpisah)
+apt install nodejs -y
+apt install ruby -y
+apt install nano -y
+apt install cowsay -y
+apt install toilet -y
+apt install neofetch -y
+apt install figlet -y
+apt install openssh -y
+apt install php -y
+apt install ffmpeg -y
+apt install imagemagick -y
+apt install rust libffi openssl build-essential -y
+apt install nmap -y
+apt install cloudflared -y
+apt install whois -y
+pip install pandas
+pip install sherlock-project
+#python3 -m pip install --upgrade pip
+#python3 -m pip install -r modul.txt #kalo di gabung, misal error satu aja malah jadi error semua :(
+pip install tqdm
+pip install scapy
+pip install ipaddress==1.0.11
+pip install Faker==5.0.0
+pip install requests urllib3 cloudscraper
+pip install beautifulsoup4
+pip install rich
+pip install inquirer
+pip install fake_useragent
+pip install asyncio
+pip install colorama
+pip install datetime
+pip install useragent
+pip install pyfiglet
+pip install bs4
+pip install random2==1.0.1
+pip install google==3.0.0
+pip install certifi
+pip install chardet==3.0.4
+pip install idna
+pip install gtts
+pip install -U setuptools wheel
+pip install python-nmap
+pip install phonenumbers
+pip install socialscan
+pip install ignorant
+pip install -U maigret #misal yg ini error abaikan, karena g support termuxl
+pip install holehe
+pip install find_github_email
+pip install user-scanner
+pip install pycryptodome
+npm install
+npm install nik-parse -g
+npm install javascript-obfuscator -g
+cd .bot && npm install
+echo -e "\n\033[1;34mInstalasi selesai! \nSilakan jalankan tools dengan perintah \033[0;37mpython3 main.py"
